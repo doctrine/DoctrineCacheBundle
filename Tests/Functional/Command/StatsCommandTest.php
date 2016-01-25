5743b3e87972df36a1457a02967bc53b1751a524
Xavier Leune <xleune@commentcamarche.net>
2016-01-25 17:06:19 +0100

    Tests fix for DoctrineCache 1.6: Purposal #2

--- Tests/Functional/Command/StatsCommandTest.php
+++ Tests/Functional/Command/StatsCommandTest.php
@@ -41,6 +41,25 @@ class StatsCommandTest extends CommandTestCase
         $this->tester->execute(array(
             'cache-name' => $this->cacheName,
         ));
-        $this->assertEquals("Stats were not provided for the {$this->cacheName} provider of type Doctrine\\Common\\Cache\\ArrayCache\n", $this->tester->getDisplay());
+
+        $stats = $this->tester->getDisplay();
+
+        if (strpos($stats, 'Stats were not') === false) {
+            // This test is for Doctrine/Cache >= 1.6.0 only
+            $this->assertStringStartsWith(
+                "Stats for the {$this->cacheName} provider of type Doctrine\\Common\\Cache\\ArrayCache:",
+                $stats
+            );
+            $this->assertContains("[hits] 0\n", $stats);
+            $this->assertContains("[misses] 0\n", $stats);
+            $this->assertRegExp('/\[uptime\] [0-9]{10}' . "\n/", $stats);
+            $this->assertContains("[memory_usage] \n", $stats);
+            $this->assertContains("[memory_available] \n", $stats);
+        } else {
+            // This test is for Doctrine/Cache < 1.6.0 only
+            $this->assertEquals("Stats were not provided for the {$this->cacheName} provider of type Doctrine\\Common\\Cache\\ArrayCache\n", $this->tester->getDisplay());
+        }
+
+
     }
 }
