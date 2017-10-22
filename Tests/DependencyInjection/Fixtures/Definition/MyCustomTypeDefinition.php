42f37d19b184520c9fedc65d0ac46343eb6dad4e
Michael Moravec <mail@majkl578.cz>
2017-10-22 19:26:13 +0200

    Remove license headers

--- Tests/DependencyInjection/Fixtures/Definition/MyCustomTypeDefinition.php
+++ Tests/DependencyInjection/Fixtures/Definition/MyCustomTypeDefinition.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Doctrine\Bundle\DoctrineCacheBundle\Tests\DependencyInjection\Fixtures\Definition;
 
 use Doctrine\Bundle\DoctrineCacheBundle\DependencyInjection\Definition\CacheDefinition;
