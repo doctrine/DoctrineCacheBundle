993740f2ae9c1f812bc4e45e2637a91ac1af59de
Baptiste Lafontaine <baptiste@nadeo.com>
2018-03-26 16:01:32 +0200

    Improve travis behavior
    - Test on PHP 7.2
    - Prefer dist over source for composer install
    - Remove HHVM
    - Use composer cache
    - Allow phpunit 5

--- Tests/travis/install-deps.sh
+++ Tests/travis/install-deps.sh
@@ -6,5 +6,12 @@ if [ "$TRAVIS_PHP_VERSION" = "hhvm" ]; then
     exit 0;
 fi
 
+VERSION_NAME=$(phpenv version-name)
+
+if [ $VERSION_NAME = "5.3" ] || [ $VERSION_NAME = "5.4" ] || [ $VERSION_NAME = "5.5" ] || [ $VERSION_NAME = "5.6" ]; then
+    echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+fi
+
 pecl install riak
 phpenv config-add $BASEDIR/php.ini
